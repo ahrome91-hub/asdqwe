06273ff440192db0ae56a6b47056a2d78dff8b38
ahrome91-hub <ahrome91@gmail.com>
2026-06-19 01:19:13 +0300

    Create db.js

---
+++ db.js
@@ -0,0 +1,12 @@
+const DB_DATA = [
+    { client: 'ИП Иванов (копия)', proposal: 'Ремонтные работы', amount: 450000, date: '2026-01-10',
+        status: 'В работе', agreed: true, comment: 'Требуют доп. согласование' },
+    { client: 'ООО ЛогистикПро', proposal: 'Транспортные услуги', amount: 670000, date: '2026-01-12',
+        status: 'Отказ', agreed: false, comment: 'Нашли дешевле' },
+    { client: 'ООО Ромашка', proposal: 'Поставка оборудования', amount: 1500000, date: '2026-01-15',
+        status: 'Отправлено', agreed: false, comment: 'Ждём ответа' },
+    { client: 'АО СтройИнвест', proposal: 'Строительные материалы', amount: 3200000, date: '2026-01-18',
+        status: 'Согласовано', agreed: true, comment: 'Договор подписан' },
+    { client: 'ООО Техносервис', proposal: 'Обслуживание ПО', amount: 890000, date: '2026-01-20',
+        status: 'Черновик', agreed: false, comment: 'Правки от клиента' }
+];
